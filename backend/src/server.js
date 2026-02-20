01f607746406974be60cec9a7236691696a8f85a
n_pvin11 <nguyenphuocvinh11102004@gmail.com>
2026-02-20 11:21:31 +0700

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRouter from "./routes/authRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
@@ -9,10 +10,19 @@ import messageRouter from "./routes/messageRoutes.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+};
 
 app.listen(3000, () => console.log(`Server running on port: ` + PORT));
